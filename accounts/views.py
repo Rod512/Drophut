3297b4ea34993d4451820200b0238f3db671cb7c
Rod512 <rudro149187@bsdi-bd.org>
2024-04-26 02:05:45 +0530

    auth form

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+
+def register(request):
+    return render(request, 'accounts/register.html')
+
+def user_login(request):
+    return render(request, 'accounts/login.html')
+
+def user_logout(request):
+    return HttpResponseRedirect('/')
--- pages/models.py
+++ pages/models.py
@@ -4,5 +4,9 @@ class Products_listing(models.Model):
     product_title = models.CharField(max_length=200)
     product_name = models.CharField(max_length=50)
     product_price = models.IntegerField()
+    old_price = models.IntegerField(default=None, null=True,)
     product_img = models.ImageField(upload_to ='photos/%Y/%m/%d/',blank=True)
+
+    def __str__(self):
+        return self.product_title
 
