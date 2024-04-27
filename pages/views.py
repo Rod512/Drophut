6501b0194bb138f61d2431a8b0fc2d321888c609
Rod512 <rudro149187@bsdi-bd.org>
2024-04-28 03:02:24 +0530

    feat: dynamic search

--- pages/views.py
+++ pages/views.py
@@ -5,3 +5,12 @@ def home(request):
     products = Products_listing.objects.all()
     return render(request, 'pages/index.html',{'products':products})
 
+def search(request):
+    if request.method == 'POST':
+        searched = request.POST['searched']
+        products = Products_listing.objects.filter(product_name__contains =searched)
+        return render(request, 'pages/index.html',{'searched':searched, 'products':products})
+    
+    else:
+        return render(request, 'pages/index.html')
+    
