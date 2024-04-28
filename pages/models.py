bf123d56de803e5e8b3de2c2adb0d6a2411184e8
Rod512 <rudro149187@bsdi-bd.org>
2024-04-29 00:08:24 +0530

    feat: single product view,login required

--- pages/models.py
+++ pages/models.py
@@ -5,7 +5,13 @@ class Products_listing(models.Model):
     product_name = models.CharField(max_length=50)
     product_price = models.IntegerField()
     old_price = models.IntegerField(default=None, null=True,)
+    product_description = models.TextField(null=True,)
     product_img = models.ImageField(upload_to ='photos/%Y/%m/%d/',blank=True)
+    photo_1 = models.ImageField(upload_to ='photos/%Y/%m/%d/',blank=True)
+    photo_2 = models.ImageField(upload_to ='photos/%Y/%m/%d/', blank=True)
+    photo_3 = models.ImageField(upload_to ='photos/%Y/%m/%d/', blank=True)
+    photo_4 = models.ImageField(upload_to ='photos/%Y/%m/%d/', blank=True)
+    photo_5 = models.ImageField(upload_to ='photos/%Y/%m/%d/', blank=True)
 
     def __str__(self):
         return self.product_title
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Products_listing
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     products = Products_listing.objects.all()
@@ -8,9 +9,16 @@ def home(request):
 def search(request):
     if request.method == 'POST':
         searched = request.POST['searched']
-        products = Products_listing.objects.filter(product_name__contains =searched)
+        products = Products_listing.objects.filter(product_name__contains = searched)
         return render(request, 'pages/index.html',{'searched':searched, 'products':products})
     
     else:
         return render(request, 'pages/index.html')
     
+@login_required(login_url='login')
+def product(request, product_id):
+    products = get_object_or_404(Products_listing, pk=product_id)
+    context = {
+        'products': products
+    }
+    return render(request,'pages/product.html',context)
