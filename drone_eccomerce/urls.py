6c4f95509690f08b6bf5985dcf41b04f6d1ce5d7
Rod512 <rudro149187@bsdi-bd.org>
2024-04-29 02:17:45 +0530

    blog app added

--- drone_eccomerce/urls.py
+++ drone_eccomerce/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("pages.urls")),
     path('', include("accounts.urls")),
+    path('', include("blog.urls")),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
 
