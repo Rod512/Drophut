3c3f5cb18343ce0b769c86a866048bb7cec10838
Rod512 <rudro149187@bsdi-bd.org>
2024-06-25 00:30:43 +0530

    cart app added

--- drone_eccomerce/urls.py
+++ drone_eccomerce/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', include("pages.urls")),
     path('', include("accounts.urls")),
     path('', include("blog.urls")),
-    path('', include('contact.urls'))
+    path('', include('contact.urls')),
+    path('', include('cart.urls'))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
 
