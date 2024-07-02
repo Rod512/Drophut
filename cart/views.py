7961d387a805192ad0c14d35ebb6e839aa60e715
Rod512 <rudro149187@bsdi-bd.org>
2024-07-03 01:39:02 +0530

    project done

--- cart/views.py
+++ cart/views.py
@@ -77,15 +77,25 @@ def cart(request,total=0, quantity=0, cart_items= None,):
     }
     return render(request, 'cart/cart.html',context)
 
-# def checkout(request,product_id):
-#     cart = Cart.objects.get(cart_id=_cart_id(request))
-#     product = get_object_or_404(Products_listing,id=product_id)
-#     cart_item = CartItem.objects.get(product=product, cart=cart)
-#     quantity = cart_item.quantity
-#     context = {
-#         'product': product,
-#         'quantity': quantity,
-#         'cart' : cart,
-#         'cart_item' : cart_item
-#     }
-#     return render(request, 'cart/checkout.html',context)
+def checkout(request, total=0, quantity=0, cart_items= None,):
+    try:
+        tax = 0
+        grand_total = 0
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart,is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.product_price * cart_item.quantity)
+            quantity += cart_item.quantity
+        tax = (5 * total) / 100
+        grand_total = total + tax
+    except ObjectDoesNotExist:
+        pass
+
+    context = {
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items,
+        'tax' : tax,
+        'grand_total': grand_total,
+    }
+    return render(request, 'cart/checkout.html',context)
